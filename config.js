7dd59f4c657fbb443df7e357341e663062584c15
Mayank8613 <83433032+Mayank8613@users.noreply.github.com>
2023-04-07 11:29:15 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+ import * as firebase from "firebase";
+
+//initialize your database
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBAQFk4D1n2RcJeqjKjALwzOqV10zNVG8k",
+
+  authDomain: "project-60-a944f.firebaseapp.com",
+
+  databaseURL: "https://project-60-a944f-default-rtdb.firebaseio.com",
+
+  projectId: "project-60-a944f",
+
+  storageBucket: "project-60-a944f.appspot.com",
+
+  messagingSenderId: "818522376056",
+
+  appId: "1:818522376056:web:7a3035223f03cfa5be2a8d"
+
+};
+
+  export default firebase.database;
+ 
+
+  
